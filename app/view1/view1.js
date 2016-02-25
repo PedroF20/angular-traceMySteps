bd86ba2a05a2c9e1435d0e7070163fe2a1731858
PedroF20 <pedro.jc.francisco@gmail.com>
2016-02-25 14:30:21 +0000

    AngularJS front-end

---
+++ app/view1/view1.js
@@ -0,0 +1,139 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope', function($scope) {
+
+	$scope.lowerValue = 0;
+	$scope.upperValue = 19;
+
+	$scope.$watch('[lowerValue, upperValue]', function () {
+		updateAllGraphs();
+	}, true);
+
+	function updateAllGraphs() {
+		createGraph("http://localhost:5000/hours", $scope.lowerValue, $scope.upperValue	, "box1");
+		createGraph("http://localhost:5000/hours", $scope.lowerValue, $scope.upperValue	, "box2");
+	}
+
+	var gridster = $(".gridster > ul").gridster({
+        widget_base_dimensions: ['auto', 140],
+        autogenerate_stylesheet: true,
+        min_cols: 1,
+        max_cols: 6,
+        widget_margins: [5, 5],
+        resize: {
+            enabled: true,
+            stop: function() {
+            	updateAllGraphs();
+            }
+        }
+    }).data('gridster');
+    $('.gridster  ul').css({'width': $(window).width()});
+
+	function createGraph(url, day_start, day_end, elementID) {
+
+		$("#" + elementID).html('<div class="handle-resize"></div>');
+
+	  d3.json(url, function (data) {
+
+
+	    var margin = {top: 20, right: 20, bottom: 40, left: 50},
+	        width = $("#" + elementID).width() - margin.left - margin.right,
+	        height = $("#" + elementID).height() - margin.top - margin.bottom,
+	        days = data.initial_hours.length;
+
+	    var x = d3.scale.linear()
+	    .domain([day_start, day_end])
+	    .range([0, width]);
+
+	    var y = d3.scale.linear()
+	    .domain([0, d3.max(data.initial_hours)])
+	    .range([height, 0]);
+
+	    var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	    var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left");
+
+	    var area1 = d3.svg.area()
+	    .x(function(d) { return x(d.day); })
+	    .y0(height)
+	    .y1(function(d) { return y(d.hour); });
+
+	    var svg = d3.select("#" + elementID).append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	    .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	    var transformation = [];
+	    data.initial_hours.forEach(function(d,i){
+	    	if(i>=day_start && i<=day_end) {
+	        	transformation.push({day: i, hour: d})
+	    	}
+	    });
+
+	    svg.append("path")
+	    .datum(transformation)
+	    .attr("class", "area1")
+	    .attr("d", area1);
+
+	    svg.append("g")
+	    .attr("class", "x" + elementID + " axis")
+	    //.attr("transform", "translate(0," + height + ")")
+	    .call(xAxis);
+
+	    svg.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis);
+
+	    $(".x" + elementID).attr("transform", "translate(0," + height + ")");
+
+	    $(document).ready(function () {
+	        $('svg path').tipsy({fallback: "Area chart representing the enter hours on a place during 19 days",
+	                             gravity: 'w'});
+	    });
+
+	  }
+
+	)};
+
+	$scope.addWidget = function() {
+		console.log("add1");
+//		var gridster = $(".gridster ul").gridster().data('gridster');
+				console.log(gridster);
+
+		gridster.add_widget.apply(gridster, ['<li>new</li>', 2, 1]);
+    	console.log("add");
+
+	}
+
+	var panelslider = null;
+	$(document).ready(function () {
+
+		$('#right-panel-link').panelslider({
+		      bodyClass: 'ps-active-right',
+		      clickClose: true,
+		      onOpen: function() {
+		        console.log('right panel open');
+		      }
+		    });
+	});
+
+
+	
+
+
+
+}]);
