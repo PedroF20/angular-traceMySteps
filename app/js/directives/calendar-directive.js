37c52757f992d91e1be0701359534380f03ac5f0
PedroF20 <pedro.jc.francisco@gmail.com>
2016-05-30 15:14:49 +0100

    Calendar update

--- app/js/directives/calendar-directive.js
+++ app/js/directives/calendar-directive.js
@@ -132,8 +132,10 @@ app.directive('calendarHeatmap', ['DataManagerService', '$rootScope', function (
          function drawChart() {
           if ( !data ) { return; }
 
-          if ( !!selected_date ) {
+          if ( !!selected_date && !!selected_date.total) {
             drawDayOverview();
+          } else if ( !!selected_date ) {
+            drawMonthOverview();
           } else {
             drawYearOverview();
           }
@@ -323,6 +325,23 @@ app.directive('calendarHeatmap', ['DataManagerService', '$rootScope', function (
                   .duration(transition_duration)
                   .ease('ease-in')
                   .style('opacity', 1);
+              })
+              .on('click', function (d) {
+                if ( in_transition ) { return; }
+  
+                in_transition = true;
+  
+                // Set selected month to the one clicked on
+                selected_date = d;
+ 
+                // Hide tooltip
+                hideTooltip();
+  
+                // Remove all year overview related items and labels
+                removeYearOverview();
+  
+                // Redraw the chart
+                drawChart();
               });
 
             // Add day labels
@@ -374,6 +393,268 @@ app.directive('calendarHeatmap', ['DataManagerService', '$rootScope', function (
 
             }, delay);
           };
+
+
+          /**
+           * Draw month overview
+           */
+          function drawMonthOverview () {
+            
+            $elem[0].svg = null;
+
+            // Define beginning and end of the month
+            var start_of_month = moment(selected_date).startOf('month');
+            var end_of_month = moment(selected_date).endOf('month');
+
+            // Filter data down to the selected month
+            var month_data = data.filter(function (d) {
+              return start_of_month <= moment(d.date) && moment(d.date) < end_of_month;
+            });           
+
+            // Define day labels and axis
+            var dayLabels = d3.time.days(moment().startOf('week'), moment().endOf('week'));
+            var dayAxis = d3.scale.ordinal()
+              .rangeRoundBands([label_padding, height], 0.1)
+              .domain(dayLabels.map(function (d) {
+                return moment(d).weekday();
+              }));
+  
+            // Define week labels and axis
+            var weekLabels = [start_of_month.week()];
+            while ( start_of_month.week() !== end_of_month.week() ) {
+              weekLabels.push(start_of_month.add(1, 'week').week());
+            }
+  
+            // Add month data items to the overview
+            var weekScale = d3.scale.ordinal()
+              .rangeRoundBands([label_padding, width], 0.1)
+              .domain(weekLabels);
+            items.selectAll('.item-block-g').remove();
+            var item_block = items.selectAll('.item-block-g')
+              .data(month_data)
+              .enter()
+              .append('g')
+              .attr('class', 'item item-block-g')
+              .attr('width', function () {
+                return (width - label_padding) / weekLabels.length - gutter * 5;
+              })
+              .attr('height', function () {
+                return Math.min(dayAxis.rangeBand(), max_block_height);
+              })
+              .attr('transform', function (d) {
+                return 'translate(' + weekScale(moment(d.date).week()) + ',' + (dayAxis(moment(d.date).weekday()) - 10) + ')';
+              })
+              .attr('total', function (d) {
+                return d.total;
+              })
+              .attr('max', function (d) {
+                return d3.max(d.summary, function (d) {
+                  return d.value;
+                });
+              })
+              .attr('date', function (d) {
+                return d.date;
+              })
+              .attr('offset', 0)
+              .on('click', function (d) {
+                if ( in_transition ) { return; }
+  
+                // Don't transition if there is no data to show
+                if ( d.total === 0 ) { return; }
+  
+                in_transition = true;
+  
+                // Set selected date to the one clicked on
+                selected_date = d;
+  
+                // Hide tooltip
+                hideTooltip();
+  
+                // Remove all month overview related items and labels
+                removeMonthOverview();
+  
+                // Redraw the chart
+                drawChart();
+              });
+  
+            var item_width = (width - label_padding) / weekLabels.length - gutter * 5;
+            var itemScale = d3.scale.linear()
+              .range([0, item_width]);
+  
+            item_block.selectAll('.item-block-rect')
+              .data(function (d) {
+                return d.summary;
+              })
+              .enter()
+              .append('rect')
+              .attr('class', 'item item-block-rect')
+              .attr('x', function (d) {
+                var total = parseInt(d3.select(this.parentNode).attr('total'));
+                var offset = parseInt(d3.select(this.parentNode).attr('offset'));
+                var item_width = d.value * ((width - label_padding) / weekLabels.length - gutter * 5) / total;
+                d3.select(this.parentNode).attr('offset', offset + item_width);
+                return offset;
+              })
+              .attr('width', function (d) {
+                var total = parseInt(d3.select(this.parentNode).attr('total'));
+                itemScale.domain([0, total]);
+                return itemScale(d.value);
+              })
+              .attr('height', function () {
+                return Math.min(dayAxis.rangeBand(), max_block_height);
+              })
+              .attr('fill', function (d) {
+                var max = parseInt(d3.select(this.parentNode).attr('max'));
+                var color = d3.scale.linear()
+                  .range(['#ffffff', '#3b6427' || '#ff4500'])
+                  .domain([-0.15 * max, max]);
+                return color(d.value) || '#ff4500';
+              })
+              .style('opacity', 0)
+              .on('mouseover', function(d) {
+                if ( in_transition ) { return; }
+
+                // Get date from the parent node
+                var date = new Date(d3.select(this.parentNode).attr('date'));
+  
+                // Construct tooltip
+                var tooltip_html = '';
+                tooltip_html += '<div class="header"><strong>' + d.name + '</strong></div><br>';
+                tooltip_html += '<div><strong>' + (d.value ? formatTime(d.value) : 'No time') + ' tracked</strong></div>';
+                tooltip_html += '<div>on ' + moment(date).format('dddd, MMM Do YYYY') + '</div>';
+  
+                // Calculate tooltip position
+                var x = weekScale(moment(date).week()) + tooltip_padding * 3;
+                while ( width - x < (tooltip_width + tooltip_padding * 3) ) {
+                  x -= 10;
+                }
+                var y = dayAxis(moment(date).weekday()) + tooltip_padding;
+  
+                // Show tooltip
+                tooltip.html(tooltip_html)
+                  .style('left', x + 'px')
+                  .style('top', y + 'px')
+                  .transition()
+                    .duration(transition_duration / 2)
+                    .ease('ease-in')
+                    .style('opacity', 1);
+              })
+              .on('mouseout', function () {
+                if ( in_transition ) { return; }
+                hideTooltip();
+              })
+              .transition()
+                .delay(function () {
+                  return ( in_transition ) ? transition_duration / 2: 0;
+                })
+                .duration(function () {
+                  return Math.cos( Math.PI * Math.random() ) * transition_duration * 2;
+                })
+               .ease('ease-in')
+                .style('opacity', 0.5)
+                .call(function (transition, callback) {
+                  if ( transition.empty() ) {
+                    callback();
+                  }
+                  var n = 0;
+                  transition
+                    .each(function() { ++n; })
+                    .each('end', function() {
+                      if ( !--n ) {
+                        callback.apply(this, arguments);
+                      }
+                    });
+                  }, function() {
+                    in_transition = false;
+                  });
+ 
+            // Add day labels
+            labels.selectAll('.label-day').remove();
+            labels.selectAll('.label-day')
+              .data(dayLabels)
+              .enter()
+              .append('text')
+              .attr('class', 'label label-day')
+              .attr('x', label_padding / 3)
+              .attr('y', function (d, i) {
+                return dayAxis(i);
+              })
+              .style('text-anchor', 'middle')
+              .attr('font-size', function () {
+                return Math.floor(label_padding / 3) + 'px';
+              })
+              .attr('dy', function () {
+                return Math.floor(width / 100) / 3;
+              })
+              .text(function (d) {
+                return moment(d).format('dddd')[0];
+              })
+              .on('mouseenter', function (d) {
+                if ( in_transition ) { return; }
+  
+                var selectedDay = moment(d);
+                items.selectAll('.item-block')
+                  .transition()
+                  .duration(transition_duration)
+                  .ease('ease-in')
+                  .style('opacity', function (d) {
+                    return (moment(d.date).day() === selectedDay.day()) ? 1 : 0.1;
+                  });
+              })
+              .on('mouseout', function () {
+                if ( in_transition ) { return; }
+  
+                items.selectAll('.item-block')
+                  .transition()
+                  .duration(transition_duration)
+                  .ease('ease-in')
+                  .style('opacity', 1);
+              });
+
+            // Add week labels
+            labels.selectAll('.label-week').remove();
+            labels.selectAll('.label-week')
+              .data(weekLabels)
+              .enter()
+              .append('text')
+              .attr('class', 'label label-week')
+              .attr('font-size', function () {
+                return Math.floor(label_padding / 3) + 'px';
+              })
+              .text(function (week_nr) {
+                return 'Week ' + week_nr;
+              })
+              .attr('x', function (d) {
+                return weekScale(d);
+              })
+              .attr('y', label_padding / 2)
+              .on('mouseenter', function (week_nr) {
+                if ( in_transition ) { return; }
+  
+                items.selectAll('.item-block')
+                  .transition()
+                  .duration(transition_duration)
+                  .ease('ease-in')
+                  .style('opacity', function (d) {
+                    return ( moment(d.date).week() === week_nr ) ? 1 : 0.1;
+                  });
+              })
+              .on('mouseout', function () {
+                if ( in_transition ) { return; }
+  
+                items.selectAll('.item-block')
+                  .transition()
+                  .duration(transition_duration)
+                  .ease('ease-in')
+                  .style('opacity', 0.75);
+              });
+              // Add button to switch back to year overview
+              drawButton();
+
+              $elem[0].svg = svg;
+
+          };
+  
 
           /**
            * Draw day overview
@@ -583,14 +864,21 @@ app.directive('calendarHeatmap', ['DataManagerService', '$rootScope', function (
               .attr('class', 'button button-back')
               .style('opacity', 0)
               .on('click', function () {
+                if ( in_transition ) { return; }
+  
+                // Set transition boolean
                 in_transition = true;
+                // Cleanup the canvas from whichever overview type was on
+                if ( !!selected_date && !!selected_date.total ) {
+                  removeDayOverview();
+                } else if ( !!selected_date ) {
+                  removeMonthOverview();
+                }
+  
                 // Unset selected date
                 selected_date = undefined;
   
-                // Remove all day overview related items and labels
-                removeDayOverview();
-  
-                // Wait for transition to finish and redraw
+                // Redraw the chart
                 drawChart();
               });
 
@@ -627,19 +915,33 @@ app.directive('calendarHeatmap', ['DataManagerService', '$rootScope', function (
               .ease('ease')
               .style('opacity', 0)
               .remove();
-            tooltip.selectAll('text')
-              .transition()
-              .duration(transition_duration)
-              .ease('ease')
-              .style('opacity', 0)
-              .remove();
             labels.selectAll('.label-day').remove();
             labels.selectAll('.label-month').remove();
 
             $elem[0].svg = svg;
           };
  
+          /**
+           * Transition and remove items and labels related to month overview
+           */
+          function removeMonthOverview() {
+            $elem[0].svg = null;
+            svg.selectAll('.item-block-rect')
+              .transition()
+              .duration(transition_duration)
+              .ease('ease-in')
+              .style('opacity', 0)
+              .attr('x', function (d, i) {
+                return (i % 2 === 0) ? -width/3 : width/3;
+              })
+              .remove();
+            labels.selectAll('.label-day').remove();
+            labels.selectAll('.label-week').remove();
+            hideBackButton();
+            $elem[0].svg = svg;
+          };
   
+   
           /**
            * Transition and remove items and labels related to daily overview
            */
@@ -658,12 +960,7 @@ app.directive('calendarHeatmap', ['DataManagerService', '$rootScope', function (
               .remove();
             labels.selectAll('.label-time').remove();
             labels.selectAll('.label-project').remove();
-            buttons.selectAll('.button')
-              .transition()
-              .duration(transition_duration)
-              .ease('ease')
-              .style('opacity', 0)
-              .remove();
+            hideBackButton();
 
             $elem[0].svg = svg;
 
@@ -680,6 +977,17 @@ app.directive('calendarHeatmap', ['DataManagerService', '$rootScope', function (
               .style('opacity', 0);
           };
 
+          /**
+           * Helper function to hide the back button
+           */
+          function hideBackButton() {
+            buttons.selectAll('.button')
+              .transition()
+              .duration(transition_duration)
+              .ease('ease')
+              .style('opacity', 0)
+              .remove();
+          };
 
           /**
            * Helper function to convert seconds to a human readable format
